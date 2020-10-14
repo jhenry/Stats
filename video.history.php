b5b5add4e6c233d74b2ffa57f6c1cbbb865e67ea
Justin Henry <justin.henry@gmail.com>
2020-10-14 17:13:43 -0400

    Play count tracking/display.

--- video.history.php
+++ video.history.php
@@ -1,6 +1,5 @@
 <?php
 $this->view->options->disableView = true;
-echo "idget:" . $_GET['videoId'];
 
 // Verify user is logged in, otherwise set the id for anonymous hits to -1
 $authService = new AuthService();
@@ -15,17 +14,25 @@ else{
 // Verify a video was selected
 if (empty($_GET['videoId']) || !is_numeric($_GET['videoId']) || $_GET['videoId'] < 1) App::Throw404();
 
+
 // Check if video is valid
+$videoMapper = new VideoMapper();
 $video = $videoMapper->getVideoByCustom(array('video_id' => $_GET['videoId'], 'status' => 'approved'));
+
 if (!$video) App::Throw404();
 
 if (is_numeric($video->videoId) && is_numeric($userId)) 
 {
-  $videoMapper = new VideoMapper();
-  $video->views++;
-  $videoMapper->save($video);
-
-  include_once "HistoryMeta.php";
+  include_once "History.php";
+  $history = new History();
   
+  $history->videoId = $video->videoId;
+  $history->userId = $userId;
+
+  include_once "HistoryMapper.php";
+  $historyMapper = new HistoryMapper();
+
+  $historyMapper->save($history);
+
 }
 ?>
